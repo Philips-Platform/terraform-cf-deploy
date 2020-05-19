f94d5dc66f9a4e88ed0c8c92386d1cafa03ef8f7
GuptaNavdeep1983 <navdeep1.gupta@gmail.com>
2020-05-19 16:06:45 -0400

    added changes to make a single microservice deployment

--- src/modules/application/outputs.tf
+++ src/modules/application/outputs.tf
@@ -2,3 +2,26 @@ output "app_instance_id" {
   value = cloudfoundry_app.instance.id
 }
 
+output "service_instances" {
+  depends_on = [
+    data.cloudfoundry_service_instance.service_instance,
+    data.cloudfoundry_service_key.service_instance_key
+  ]
+  value = {
+    for service in data.cloudfoundry_service_instance.service_instance :
+    service.name => service.id
+  }
+
+
+  description = "services_map"
+}
+output "service_key_credentials" {
+  depends_on = [
+    data.cloudfoundry_service_key.service_instance_key
+  ]
+  value = {
+    for service in data.cloudfoundry_service_key.service_instance_key :
+    service.name => service.credentials
+  }
+  description = "services_credentials"
+}
---
+++ src/outputs.tf
@@ -0,0 +1,5 @@
+output "sample_app" {
+  value = [
+    module.gradle-sample-app.service_instances
+  ]
+}
