e1dd5e9fec424b1a59f92db7966e301105233abf
GuptaNavdeep1983 <navdeep1.gupta@gmail.com>
2020-05-26 16:14:40 -0400

    reverted change

--- src/modules/space/main.tf
+++ src/modules/space/main.tf
@@ -1,8 +1,6 @@
 resource "cloudfoundry_space" "space" {
-  name       = lower(var.space_name)
-  org        = data.cloudfoundry_org.org.id
-  managers   = var.space_users
-  developers = var.space_users
+  name = lower(var.space_name)
+  org  = data.cloudfoundry_org.org.id
 }
 data "cloudfoundry_org" "org" {
   name = var.org_name
