bbcddafa3bc7b7f4082a035edb882159f9745774
GuptaNavdeep1983 <navdeep1.gupta@gmail.com>
2020-05-26 10:03:22 -0400

    fixing CF users issues.

--- src/modules/space/main.tf
+++ src/modules/space/main.tf
@@ -1,6 +1,8 @@
 resource "cloudfoundry_space" "space" {
-  name = lower(var.space_name)
-  org  = data.cloudfoundry_org.org.id
+  name       = lower(var.space_name)
+  org        = data.cloudfoundry_org.org.id
+  managers   = var.space_users
+  developers = var.space_users
 }
 data "cloudfoundry_org" "org" {
   name = var.org_name
