fbcfc6af18d7c3da71b39c1df2d24d2cd0a2f02e
Todd <jason.todd@philips.com>
2020-05-29 12:25:04 -0400

    deprecated, don't need it

--- src/modules/space/main.tf
+++ src/modules/space/main.tf
@@ -1,12 +1,12 @@
 resource "cloudfoundry_space" "space" {
   name = lower(var.space_name)
   org  = data.cloudfoundry_org.org.id
-  managers = var.space_users
-  developers = var.space_users
 }
+
 data "cloudfoundry_org" "org" {
   name = var.org_name
 }
+
 resource "cloudfoundry_space_users" "space" {
   space      = cloudfoundry_space.space.id
   managers   = var.space_users
